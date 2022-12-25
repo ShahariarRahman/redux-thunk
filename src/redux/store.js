9d5ed502ddedeb668f09cd0debed61649e86af83
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 01:28:38 +0600

    redux middleware: redux-logger used in applyMiddleware() to show action in console

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
-
+import logger from 'redux-logger'
 // composeWithDevTools()
-const store = createStore(rootReducer, applyMiddleware());
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
+
+// npm i redux-logger
