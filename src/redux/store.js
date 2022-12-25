4b11cdcc6e93770d75471ad11a6ba706bee0c98b
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 03:37:59 +0600

    redux-thunk installed && multiple middleware add{cartCounter,thunk,logger}

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 import cartCounter from "./middleware/cartCounter";
-
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(cartCounter))
+    composeWithDevTools(applyMiddleware(cartCounter, thunk, logger))
 );
 
 export default store;
