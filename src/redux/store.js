6d34cda74a8d825d7028b8aac3e68e2ef0d6c841
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 02:31:53 +0600

    custom middleware using curry function, we can get current state by store.getState() && next && action;;; if return next(action) then the dispatch execute otherwise the dispatch will stopped by middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
-import logger from 'redux-logger';
+import cartCounter from "./middleware/cartCounter";
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(logger))
+    composeWithDevTools(applyMiddleware(cartCounter))
 );
 
 export default store;
