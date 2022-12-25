759fa38214e36f26cf8605e3c7d11a791d802f3b
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 03:09:43 +0600

    sort cart::: cartPosition:cart.length at action.payload

--- src/redux/middleware/cartCounter.js
+++ src/redux/middleware/cartCounter.js
@@ -1,7 +1,18 @@
-const cartCounter = (store) => (next) => (action) => {
-    console.log('Current State', store.getState());
-    console.log('Action', action);
+import { ADD_TO_CART } from "../actionTypes/actionTypes";
 
+const cartCounter = (store) => (next) => (action) => {
+    const state = store.getState();
+    const cart = state.product.cart;
+    if (action.type === ADD_TO_CART) {
+        const newAction = {
+            ...action,
+            payload: {
+                ...action.payload,
+                cartPosition: cart.length
+            },
+        };
+        return next(newAction);
+    }
     return next(action);
 };
 
