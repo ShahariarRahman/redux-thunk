aefff446aa6f5aba8759990576bb0f4affb8ffd4
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 01:33:19 +0600

    redux middleware: redux-logger middleware used in composeWithDevTools to use middleware & devtools both

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 import logger from 'redux-logger'
-// composeWithDevTools()
-const store = createStore(rootReducer, applyMiddleware(logger));
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger))
+);
 
 export default store;
-
-// npm i redux-logger
