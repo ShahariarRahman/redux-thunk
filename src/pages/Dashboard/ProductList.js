0380c39601955422b62c871fee0b1d65d047c11e
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-26 07:47:06 +0600

    Handling 'add product' form locally

--- src/pages/Dashboard/ProductList.js
+++ src/pages/Dashboard/ProductList.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import loadProductData from "../../redux/thunk/products/fetchProducts";
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
-
+  const products = useSelector(state => state.product.products);
+  const dispatch = useDispatch();
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.data));
-  });
+    dispatch(loadProductData());
+  }, [dispatch]);
 
   return (
     <div class='flex flex-col justify-center items-center h-full w-full '>
--- src/redux/actionTypes/actionTypes.js
+++ src/redux/actionTypes/actionTypes.js
@@ -1,8 +1,8 @@
-export const ADD_PRODUCT = "ADD_PRODUCT";
 export const ADD_TO_CART = "ADD_TO_CART";
 export const PRODUCT_LOADED = "PRODUCT_LOADED";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const LOAD_PRODUCT = "LOAD_PRODUCT";
+export const ADD_PRODUCT = "ADD_PRODUCT";
 
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
 // For filters
